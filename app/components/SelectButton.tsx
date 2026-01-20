0eb81a5212d986d2bda3434274f9017208756047
Atish Kumar <aatishece09@gmail.com>
2026-01-20 11:57:52 +0530

    Add desktop design

---
+++ app/components/SelectButton.tsx
@@ -0,0 +1,31 @@
+interface SelectButtonProps {
+  label: string
+  value: string
+  selectedValue: string | null
+  onClick: (value: string) => void
+}
+
+export default function SelectButton({
+  label,
+  value,
+  selectedValue,
+  onClick,
+}: SelectButtonProps) {
+  const isSelected = selectedValue === value
+
+  return (
+    <button
+      onClick={() => onClick(value)}
+      className={`w-full bg-white rounded-4xl p-[18px] my-[13px] text-center border transition cursor-pointer ${
+        isSelected
+          ? 'border-blue-500 border-2'
+          : 'border-[#E4E4E7]'
+      }`}
+    >
+      <p className="text-sm font-semibold text-black">
+        {label}
+      </p>
+    </button>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,35 @@
 'use client'
 
-import Link from 'next/link'
+
 import { useState,useEffect } from 'react'
 import { Cog, Check } from 'lucide-react'
+import SelectButton from './components/SelectButton'
 
 export default function Home() {
   const [currentStep, setCurrentStep] = useState(1)
   const [mounted, setMounted] = useState(0)
-  const [selectedAction, setSelectedAction] = useState<string | null>(null)
+  const [selectedChallenge, setSelectedChallenge] = useState<string | null>(null)
   const [selectedRevenue, setSelectedRevenue] = useState<string | null>(null)
   const [websiteUrl, setWebsiteUrl] = useState('')
   const [email, setEmail] = useState('')
-  const [rulesCount, setRulesCount] = useState(0)
   const [showAnalyze, setShowAnalyze] = useState(false)
 
   const totalSteps = 3
+
+  // Step 1 buttons data
+  const step1Buttons = [
+    { value: 'low-conversion-rates', label: 'Low conversion rates' },
+    { value: 'low-average-order-value', label: 'Low average order value' },
+    { value: 'both', label: 'Both' },
+  ]
+
+  // Step 2 buttons data
+  const step2Buttons = [
+    { value: 'under-10k', label: 'Under €10,000 / month' },
+    { value: '10k-50k', label: '€10,000–€50,000 / month' },
+    { value: '50k-100k', label: '€50,000–€100,000 / month' },
+    { value: 'over-100k', label: 'Over €100,000 / month' },
+  ]
 
   const handleNext = () => {
     if (currentStep < totalSteps) {
@@ -42,8 +57,6 @@ export default function Home() {
       return
     }
 
-    // Reset to 0 when analyze starts
-    setMounted(0)
     
     const interval = setInterval(() => {
       setMounted((prev) => {
@@ -60,30 +73,40 @@ export default function Home() {
 
   const progressPercentage = (currentStep / totalSteps) * 100
 
+  // Check if current step has required value
+  const isStepValid = () => {
+    if (currentStep === 1) {
+      return selectedChallenge !== null
+    }
+    if (currentStep === 2) {
+      return selectedRevenue !== null
+    }
+    if (currentStep === 3) {
+      return websiteUrl.trim() !== '' && email.trim() !== ''
+    }
+    return false
+  }
+
   return (
-    <main className="min-h-screen flex items-center justify-center md:px-4 bg-[#FDFDFD]">
-      <div className=" w-full max-w-[720px] min-h-screen md:min-h-fit md:rounded-2xl md:shadow-xl/10 md:border md: border-[#E4E4E7] overflow-hidden">
+    <main className=" flex items-center justify-center md:px-4 bg-[#FDFDFD]">
+      <div className="max-w-[400px] mx-[16px]">
         {/* Header with Logo and Progress */}
         {!showAnalyze && (
-        <div className="px-6 my-3">
+        <>
           {/* Logo */}
-          <div className="text-center my-4">
-            <h1 className="text-2xl font-bold text-gray-900">
-              <span className="font-bold">CXO</span>
-              <span className="font-normal text-gray-600">studio</span>
-        </h1>
+          <div className="text-center my-[34px]">
+          <img src="/cxo_logo.png" alt="logo" className="mx-auto object-cover w-[117.54]  h-[19.95] object-cover" />
           </div>
           
           {/* Back Button and Progress Bar */}
           <div className="flex items-center gap-3">
-            {currentStep > 1 && (
+
               <button
                 onClick={handleBack}
-                className="w-10 h-10 rounded-lg bg-white border-[1px] border-[#E4E4E7] flex items-center justify-center hover:bg-gray-200 transition shrink-0 cursor-pointer"
+                className="w-[35px] h-[35px] rounded-lg bg-white border border-[#E4E4E7] flex items-center justify-center hover:bg-gray-200 transition shrink-0 cursor-pointer"
               >
                 <span className="text-gray-700 text-xl">‹</span>
               </button>
-            )}
             {/* Progress Bar */}
             <div className="flex-1 bg-gray-200 rounded-full h-2">
               <div
@@ -92,129 +115,62 @@ export default function Home() {
               ></div>
             </div>
           </div>
-        </div>
+        </>
         )}
         {/* Step Content */}
-        <div className="my-[33px] mx-[16px]">
+        <div className="my-[35px] mx-[16px]">
           {!showAnalyze ? (
             <>
               {currentStep === 1 && (
                 <div>
-                  <h2 className="text-3xl font-bold text-gray-900 text-center">
+                  <h2 className="text-[33px] leading-[48px] font-bold text-gray-[#09090B] text-center">
                     What's your biggest challenge right now?
                   </h2>
-                  <div className="mt-[43px]">
-                    <button
-                      onClick={() => setSelectedRevenue('low-conversion-rates')}
-                      className={`w-full bg-white rounded-4xl p-[18px] my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === 'low-conversion-rates'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        Low conversion rates
-                      </p>
-                    </button>
-                    <button
-                      onClick={() => setSelectedRevenue('low-average-order-value')}
-                      className={`w-full bg-white rounded-4xl p-[18px] my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === 'low-average-order-value'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        Low average order value
-                      </p>
-                    </button>
-                    <button
-                      onClick={() => setSelectedRevenue('both')}
-                      className={`w-full bg-white rounded-4xl p-[18px] my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === 'both'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        Both
-                      </p>
-                    </button>
+                  <div className="mt-[28px]">
+                    {step1Buttons.map((button) => (
+                      <SelectButton
+                        key={button.value}
+                        label={button.label}
+                        value={button.value}
+                        selectedValue={selectedChallenge}
+                        onClick={setSelectedChallenge}
+                      />
+                    ))}
                   </div>
                 </div>
               )}
 
               {currentStep === 2 && (
-                <div className="mt-[33px]">
-                  <h2 className="text-3xl font-bold text-gray-900 text-center px-4">
+                <>
+                  <h2 className="text-3xl my-[33px] font-bold text-gray-900 text-center px-4">
                     What's your average online revenue?
                   </h2>
                   <div className="mt-[43px]">
-                    <button
-                      onClick={() => setSelectedRevenue('under-10k')}
-                      className={`w-full bg-white rounded-4xl p-4 my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === 'under-10k'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        Under €10,000 / month
-                      </p>
-                    </button>
-                    <button
-                      onClick={() => setSelectedRevenue('10k-50k')}
-                      className={`w-full bg-white rounded-4xl p-4 my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === '10k-50k'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        €10,000–€50,000 / month
-                      </p>
-                    </button>
-                    <button
-                      onClick={() => setSelectedRevenue('50k-100k')}
-                      className={`w-full bg-white rounded-4xl p-4 my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === '50k-100k'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        €50,000–€100,000 / month
-                      </p>
-                    </button>
-                    <button
-                      onClick={() => setSelectedRevenue('over-100k')}
-                      className={`w-full bg-white rounded-4xl p-4 my-[13px] text-center border transition cursor-pointer ${
-                        selectedRevenue === 'over-100k'
-                          ? 'border-blue-500 border-2'
-                          : 'border-[#E4E4E7]'
-                      }`}
-                    >
-                      <p className="text-sm font-semibold text-black">
-                        Over €100,000 / month
-                      </p>
-                    </button>
+                    {step2Buttons.map((button) => (
+                      <SelectButton
+                        key={button.value}
+                        label={button.label}
+                        value={button.value}
+                        selectedValue={selectedRevenue}
+                        onClick={setSelectedRevenue}
+                      />
+                    ))}
                   </div>
-                </div>
+                </>
               )}
 
               {currentStep === 3 && (
-                <div className="my-[33px]">
+                <>
                   <div>
-                    <h2 className="text-[33px] leading-[40px] font-bold text-[#757575] text-center">
+                    <h2 className="text-[33px] leading-[48px] font-bold text-[#757575] text-center">
                       You're almost done!
                     </h2>
-                    <h2 className="text-[33px] leading-[40px] font-bold text-black text-center">
+                    <h2 className="text-[33px] leading-[48px] font-bold text-black text-center">
                       Let's finish your audit
                     </h2>
                   </div>
-                  <div className="mt-8">
-                    <div>
-                      <label className="block text-sm font-semibold text-black mb-3">
+                  <div className="mt-[33px]">
+                      <label className="block text-sm font-semibold text-black">
                         Website URL:
                       </label>
                       <input
@@ -222,11 +178,10 @@ export default function Home() {
                         value={websiteUrl}
                         onChange={(e) => setWebsiteUrl(e.target.value)}
                         placeholder="Enter the URL of your main product page"
-                        className="w-full px-4 py-3 border border-gray-300 rounded-xl bg-white text-sm focus:outline-none"
+                        className="w-full mt-[13px] px-4 py-3 border border-gray-300 rounded-xl bg-white text-sm focus:outline-none"
                       />
-                    </div>
-                    <div className="relative mt-[18px]">
-                      <label className="block text-sm font-semibold text-black mb-3">
+                    <div className="relative mt-[19px]">
+                      <label className="block text-sm font-semibold text-black">
                         Email address:
                       </label>
                       <input
@@ -234,25 +189,30 @@ export default function Home() {
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="Enter your best email address"
-                        className="w-full px-4 py-3 border border-gray-300 rounded-xl bg-white text-sm focus:outline-none"
+                        className="w-full mt-[12px] px-4 py-3 border border-gray-300 rounded-xl bg-white text-sm focus:outline-none"
                       />
                     </div>
                   </div>
-                </div>
+                </>
               )}
 
               {/* Continue Button or Access Results Button */}
               {currentStep < totalSteps ? (
-                <div className="mt-8">
+                <div className="mt-[91px]">
                   <button
                     onClick={handleNext}
-                    className="w-full py-[18px] rounded-xl transition font-semibold text-sm text-center bg-black text-white hover:bg-gray-800 cursor-pointer"
+                    disabled={!isStepValid()}
+                    className={`w-full py-[18px] rounded-xl transition font-semibold text-sm text-center cursor-pointer ${
+                      !isStepValid()
+                        ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+                        : 'bg-black text-white hover:bg-gray-800'
+                    }`}
                   >
                     Continue ›
                   </button>
                 </div>
               ) : (
-                <div className="mt-8">
+                <div className="mt-[91px]">
                   <button
                     onClick={() => {
                       if (websiteUrl && email) {
@@ -283,12 +243,12 @@ export default function Home() {
           </h2>
 
       {/* Phone */}
-      <div className="flex justify-center mb-8">
-        <img src="/IPhone.png" className="w-full max-w-xs drop-shadow-2xl" />
+      <div className="flex justify-center">
+        <img src="/IPhone.png" className="w-[428px] h-[321px] object-cover" />
       </div>
 
       {/* Steps */}
-      <div className="space-y-3">
+      <div className="mt-[45px]">
         {analysisSteps.map((title, index : number) => {
           const isCompleted = index < mounted
           const isActive = index === mounted
@@ -297,7 +257,7 @@ export default function Home() {
           return (
             <div
               key={index}
-              className={`flex items-center gap-4 p-4 rounded-xl border transition-all duration-700 ${
+              className={`flex items-center gap-4 p-4 my-[14px] rounded-xl border transition-all duration-700 ${
                 isCompleted
                   ? `border-green-500 ${shouldAnimateOut ? 'step-completed slide-out-right' : ''}`
                   : isActive
@@ -306,16 +266,16 @@ export default function Home() {
               }`}
             >
               {isCompleted ? (
-                <div className="w-6 h-6 rounded-full bg-green-500 flex items-center justify-center">
-                  <Check className="w-4 h-4 text-white" />
+                <div className="w-5 h-5 rounded-full bg-green-500 flex items-center justify-center">
+                  <Check className="w-5 h-5 text-white" />
                 </div>
               ) : (
-                <Cog className={`w-6 h-6 ${isActive ? 'text-black' : 'text-gray-400'}`} />
+                <Cog className={`w-5 h-5 ${isActive ? 'text-black' : 'text-gray-400'}`} />
               )}
 
               <span
                 className={`${
-                  isActive ? 'text-black font-medium' : 'text-gray-400'
+                  isActive ? 'text-black  font-semibold text-[14.8px] leading-[28.8px]' : 'text-gray-400 font-semibold text-[14.8px] leading-[28.8px]'
                 } ${isCompleted ? 'line-through' : ''}`}
               >
                 {title}
@@ -330,18 +290,18 @@ export default function Home() {
 
         {/* Social Proof Footer - Only show when not analyzing */}
         {!showAnalyze && (
-          <div className="my-[18px] border-t border-gray-100">
+          <div className="my-[18px]">
             <div className="flex justify-center gap-3">
               {/* Left: Profile Images */}
               <div className="flex -space-x-2">
                 <div className="w-10 h-10 rounded-full border-2 border-white overflow-hidden bg-gray-200">
-                  <img src="/client_first.png" alt="user" className="w-full h-full object-cover" />
+                  <img src="/client_first.png" alt="user" className="w-[40px] h-[40px] object-cover" />
                 </div>
                 <div className="w-10 h-10 rounded-full border-2 border-white overflow-hidden bg-gray-200">
-                  <img src="/client_second.png" alt="user" className="w-full h-full object-cover" />
+                  <img src="/client_second.png" alt="user" className="w-[40px] h-[40px] object-cover" />
                 </div>
                 <div className="w-10 h-10 rounded-full border-2 border-white overflow-hidden bg-gray-200">
-                  <img src="/client_third.png" alt="user" className="w-full h-full object-cover" />
+                  <img src="/client_third.png" alt="user" className="w-[40px] h-[40px] object-cover" />
                 </div>
               </div>
               
@@ -349,10 +309,10 @@ export default function Home() {
               <div className="flex flex-col">
                 <div className="flex items-center gap-1">
                   {[...Array(5)].map((_, i) => (
-                    <span key={i} className="text-orange-400 text-lg">★</span>
+                    <span key={i} className="text-[#FFB66E] text-lg w-[16px] h-[16px]">★</span>
                   ))}
                 </div>
-                <p className="text-xs text-gray-500">
+                <p className="text-xs font-semibold text-[#71717A] mt-[4px]">
                   Trusted by e-commerce founders
                 </p>
               </div>
