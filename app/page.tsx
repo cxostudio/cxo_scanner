0a769ef69867815107efd81ed536d4187fb49134
cxostudio <info@cxo.studio>
2026-02-09 17:07:32 +0530

    add the website in the  result section

--- app/page.tsx
+++ app/page.tsx
@@ -612,6 +612,8 @@ export default function Home() {
                   </p>
                 </motion.div>
               </div>
+              
+              
 
               {/* Steps */}
               <div className="mt-[24px]">
